9d8ab5228c8f396e3cbbd8227debf095a11b8d6d
David Brown and Paul Harbinson <pair+dbrox+pha4f@allstate.com>
2016-05-02 15:07:25 +0100

    Working version 1

--- log_a_run/test/acceptance/my_runs_test.js
+++ log_a_run/test/acceptance/my_runs_test.js
@@ -26,26 +26,43 @@ describe('Given I am on the my-runs page', function() {
   it('Then i should see a header with "my name runs" and a button to log a run and list containing my runs', function(done) {
     var user = {name: 'users name', email: 'email@email.com', password: 'password1'}
 
-    userCollection.insert(user).then(function(savedUser) {
-      browser.get('/my-runs/' + savedUser._id)
+    userCollection.insert(user)
+    .then(function(savedUser) {
+      var run = {userid: savedUser._id, name: 'run name', location: 'location of run', distance: '100', time: '200'}
 
-      var header = element(by.id('header'))
-      var logmyrun_button = element(by.id('logmyrun'))
-      var myrunslist = element(by.id('runlist'))
+      return run
+    })
+    .then(function(run) {
+        runCollection.insert(run).then(function(savedRun) {
 
-      header.getText().then(function(text){
-        expect(text).to.equal('My Runs')
-      })
-
-      logmyrun_button.getTagName().then(function(tagname){
-        expect(tagname).to.equal('a')
-      })
-
-      myrunslist.getText().then(function(runs){
-        expect(runs).to.equal('List of Runs')
-
-        done()
+          return savedRun
       })
     })
+    // .then(function(savedRun) {
+    //   browser.get('/my-runs/' + savedUser._id)
+    //
+    //   var header = element(by.id('header'))
+    //   var logmyrun_button = element(by.id('logmyrun'))
+    //   var myrunslist = element(by.id('runlist'))
+    //   var run_listcontent = element(by.tagName('td'))
+    //
+    //   header.getText().then(function(text){
+    //     expect(text).to.equal('My Runs')
+    //   })
+    //
+    //   logmyrun_button.getTagName().then(function(tagname){
+    //     expect(tagname).to.equal('a')
+    //   })
+    //
+    //   myrunslist.getText().then(function(runs){
+    //     expect(runs).to.equal('List of Runs')
+    //   })
+    //
+    //   run_listcontent.getText().then(function(runs) {
+    //     expect(runs).to.equal('run name')
+    //
+    //     done()
+    //   })
+    // })
   })
 })
